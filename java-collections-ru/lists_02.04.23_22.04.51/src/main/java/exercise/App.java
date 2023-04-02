976b4bdb5058c50d8df7dd621d23948eb5fd0119
@hexlet/cli <support@hexlet.io>
2023-04-02 22:07:39 +0700

    submit java-collections-ru/lists_02.04.23_22.04.51

---
+++ java-collections-ru/lists_02.04.23_22.04.51/src/main/java/exercise/App.java
@@ -0,0 +1,22 @@
+package exercise;
+
+// BEGIN
+public class App {
+    public static boolean scrabble(String letters, String word) {
+        char[] arr = letters.toCharArray();
+        word = word.toLowerCase();
+        boolean isScrabblable = true;
+        List<Character> list = new ArrayList<>(arr);
+        for (int i = 0; i < word.length(); i++) {
+            char current = word.charAt(i);
+            if (list.contains(current)) {
+                int index = list.indexOf(current);
+                list.remove(index);
+            } else {
+                isScrabblable = false;
+            }
+        }
+        return isScrabblable;
+    }
+}
+//END
