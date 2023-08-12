3c8171abb9473c53cb3691dbcff63c50c6006c6f
@hexlet/cli <support@hexlet.io>
2023-08-13 01:35:50 +0700

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -0,0 +1,22 @@
+package exercise;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+// BEGIN
+public class App {
+    public static void save(Path path, Car car) throws IOException {
+        Files.writeString(path, car.serialize());
+    }
+
+    public static Car extract(Path path) throws IOException {
+        Car result = Car.unserialize(Files.readString(path));
+        return result;
+    }
+}
+// END
