04428b14fb293d89ff6daf1570f7d5da8159c5e4
@hexlet/cli <support@hexlet.io>
2023-12-17 05:49:35 +0700

    submit java-spring-ru/response-entity

---
+++ java-spring-ru/response-entity/src/main/java/exercise/Application.java
@@ -0,0 +1,68 @@
+package exercise;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import exercise.model.Post;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> index(@RequestParam(defaultValue = "10") Integer limit) {
+        var result = posts.stream().limit(limit).toList();
+
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(posts.size()))
+                .body(result);
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Post> find(@PathVariable String id) {
+        var post = posts.stream().filter(p -> p.getId().equals(id)).findFirst();
+        return ResponseEntity.of(post);
+    }
+
+    @PostMapping("/posts")
+    public ResponseEntity<Post> create(@RequestBody Post post) {
+        posts.add(post);
+        return ResponseEntity.status(201).body(post);
+    }
+
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<Post> update(@PathVariable String id, @RequestBody Post data) {
+        var prpost = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (prpost.isPresent()) {
+            var post = prpost.get();
+            post.setBody(data.getBody());
+            post.setTitle(data.getTitle());
+            post.setId(data.getId());
+            return ResponseEntity.ok(post);
+        } else {
+            return ResponseEntity.status(204).body(data);
+        }
+    }
+    // END
+
+    @DeleteMapping("/posts/{id}")
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
+}
